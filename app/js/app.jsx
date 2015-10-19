264ae099eb23d08a54e603f57dd23babc2c0ae42
Wade Mauger <wadeanthony0100@gmail.com>
2015-10-19 14:07:57 -0400

    initil implementation of a quote in react

--- app/js/app.jsx
+++ app/js/app.jsx
@@ -20,6 +20,7 @@ class App extends React.Component {
                         qdbid='1129'
                         quoteText='Hello World with React!'
                         smallText='Hello World subtext!'
+                        tags={['this tag', 'this other tag', 'and also this one']}
                     />
                 </div>
             </div>
--- app/js/quote.js
+++ app/js/quote.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 
 export default class Quote extends Component {
 	render() {
+        var tags = this.props.tags
         return(
 		<div className='well well-lg'>
             <a href='#'>{this.props.qdbid}</a>
@@ -12,7 +13,9 @@ export default class Quote extends Component {
                 {this.props.smallText}
             </small>
             <hr />
-
+            {tags.map(function(tag){
+                return <button type='button' className='qdb-tag-button btn btn-primary btn-sm' id='{tag}'>{tag}</button> ;                                  
+            })}
         </div>
         );
 	}
