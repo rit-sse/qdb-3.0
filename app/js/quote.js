9aa9eeeac7702ee7c7f977cf92ad5fb4920b83fd
Wade Mauger <wadeanthony0100@gmail.com>
2015-10-19 11:07:51 -0400

    add text and id link to quote well

--- app/js/quote.js
+++ app/js/quote.js
@@ -5,6 +5,9 @@ export default class Quote extends Component {
         return(
 		<div className="well well-lg">
             <a href="#">{this.props.qdbid}</a>
+            <blockquote className="qdb-quote-text">
+                {this.props.quoteText}
+            </blockquote>
         </div>
         );
 	}
