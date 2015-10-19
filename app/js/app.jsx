9f2aa3bacb07055cb1826bce6006d202239234f4
Wade Mauger <wadeanthony0100@gmail.com>
2015-10-19 10:21:09 -0400

    initial implementation of a quote

--- app/js/app.jsx
+++ app/js/app.jsx
@@ -2,10 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import Navbar from './navbar';
+import Quote from './quote';
 
 class App extends React.Component {
     render() {
-        return(<Navbar />);
+        return(
+            <div>    
+                <Navbar />
+                <div className="container" id="qdb-content" qdbid="1129">
+                    <Quote />
+                </div>
+            </div>
+        );
     }
 }
 
