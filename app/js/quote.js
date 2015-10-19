68e589941afe55cba248aaf928c76a8b5799508a
Wade Mauger <wadeanthony0100@gmail.com>
2015-10-19 13:22:29 -0400

    improve code style, add application.scss

--- app/js/quote.js
+++ app/js/quote.js
@@ -3,11 +3,16 @@ import React, {Component} from 'react';
 export default class Quote extends Component {
 	render() {
         return(
-		<div className="well well-lg">
-            <a href="#">{this.props.qdbid}</a>
-            <blockquote className="qdb-quote-text">
+		<div className='well well-lg'>
+            <a href='#'>{this.props.qdbid}</a>
+            <blockquote className='qdb-quote-text'>
                 {this.props.quoteText}
             </blockquote>
+            <small id='qdb-small-text'>
+                {this.props.smallText}
+            </small>
+            <hr />
+
         </div>
         );
 	}
