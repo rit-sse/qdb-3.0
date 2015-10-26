1d7c484ef700e7e66e9f2dd9c15ad51d461f93a7
Wade Mauger <wadeanthony0100@gmail.com>
2015-10-26 13:18:56 -0400

    refactored tags initialization in tags-list.js

--- app/js/components/tags-list.js
+++ app/js/components/tags-list.js
@@ -5,12 +5,8 @@ import Tag from './tag';
 
 export default class TagsList extends Component {
     render() {
-      let tags = this.props.tags;
-      if (typeof tags !== 'undefined') {
-        tags.sort();
-      } else {
-        tags = [];
-      }
+      let tags = this.props.tags || [];
+      tags.sort();
       let tagElements = tags.map((thisTag) => <Tag key={'qdb-tag-' + thisTag}  tag={thisTag} /> );
       return(
               <div>
