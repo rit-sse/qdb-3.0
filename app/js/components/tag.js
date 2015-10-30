bf9e48febb168d8a152a56ffc71377ec0bacb331
Wade Mauger <wadeanthony0100@gmail.com>
2015-10-30 10:47:51 -0400

    change single quotes to backticks in tags.js

--- app/js/components/tag.js
+++ app/js/components/tag.js
@@ -5,7 +5,7 @@ import React, { Component } from 'react';
 export default class Tag extends Component {
     render() {
       return(
-            <button className='qdb-tag-button btn btn-primary btn-sm' id={'qdb-tag-${this.props.tag}'}>{this.props.tag}</button>
+            <button className='qdb-tag-button btn btn-primary btn-sm' id={`qdb-tag-${this.props.tag}`}>{this.props.tag}</button>
         );
     }
 }
