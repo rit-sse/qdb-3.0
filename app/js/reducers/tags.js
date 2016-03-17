1f1ccc8bcc91e82c3f48ece4794cdeed5ed15a49
Kristen Mills <kristen@kristen-mills.com>
2016-03-16 23:02:40 -0700

    Sort tags

--- app/js/reducers/tags.js
+++ app/js/reducers/tags.js
@@ -1,8 +1,20 @@
 import { GET_TAGS_SUCCESS } from '../actions/tags';
 
+function compare(a, b) {
+  if (a.name < b.name) {
+    return -1;
+  }
+
+  if (a.name > b.name ) {
+    return 1;
+  }
+  return 0;
+}
+
 export default function tags(state = [], action) {
   switch (action.type) {
   case GET_TAGS_SUCCESS:
+    action.tags.sort(compare);
     return action.tags;
   default:
     return state;
