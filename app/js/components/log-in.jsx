70d4289ddddb4e943a4a4ad1aa7ccb62b28e949d
Dylan Vuz <vuzdylan@gmail.com>
2016-03-29 10:41:17 -0400

    welp some one didn't merge correctly

--- app/js/components/log-in.jsx
+++ app/js/components/log-in.jsx
@@ -10,7 +10,6 @@ export default class LogIn extends Component {
 
   componentDidMount() {
     // Retrieve the singleton for the GoogleAuth library and set up the client.
-<<<<<<< HEAD
     API
       .Auth
       .clientId()
@@ -24,24 +23,11 @@ export default class LogIn extends Component {
          });
       }) 
       .catch( err => console.log(err) );
-=======
-    const auth2 = gapi.auth2.init({
-      client_id: process.env.GOOGLE_CLIENT_ID, // eslint-disable-line camelcase
-      cookie_policy: 'single_host_origin', // eslint-disable-line camelcase
-    });
-    auth2.attachClickHandler(this.refs.button, {}, googleUser => {
-      this.props.dispatch(signIn(googleUser));
-    });
->>>>>>> 5d1ffc0ec5cec44e89e1123ed7e44f25427403e4
   }
 
   render() {
     return (
-<<<<<<< HEAD
       <button disable={this.state.disable_login} id='login' className='btn btn-primary navbar-btn ' ref='button'>
-=======
-      <a href='#' id='login' ref='button'>
->>>>>>> 5d1ffc0ec5cec44e89e1123ed7e44f25427403e4
         <i className='fa fa-google'></i> Sign In
       </a>
     );
