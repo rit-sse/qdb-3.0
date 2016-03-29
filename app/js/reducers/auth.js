9e159d78fa7bc72672f6ddbca23ba12301fb7577
Dylan Vuz <vuzdylan@gmail.com>
2016-03-29 10:03:42 -0400

    removed annoying notification

--- app/js/reducers/auth.js
+++ app/js/reducers/auth.js
@@ -1,13 +1,17 @@
 import {
   SIGN_IN_SUCCESS,
   SIGN_OUT_SUCCESS,
+  SIGN_IN_BY_TOKEN,
+  SIGN_OUT_BY_TOKEN
 } from '../actions/auth';
 
 export default function auth(state = { signedIn: false }, action) {
   switch (action.type) {
   case SIGN_IN_SUCCESS:
+  case SIGN_IN_BY_TOKEN:
     return { signedIn: true, officer: action.user.officer  };
   case SIGN_OUT_SUCCESS:
+  case SIGN_OUT_BY_TOKEN:
     return { signedIn: false };
   default:
     return state;
